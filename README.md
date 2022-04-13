25906143410eb8c03f5f60a94557ea27def5a0ab
Pazzo97 <pazzomuk@gmail.com>
2022-04-13 21:13:26 +0200

    Add html css and javascript file

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Capstone
+# Nobilis Summer Bootcamp
+
+> This project is about the Nobilis summer bootcamp which teach about website development using Html5, CSS3, and Javascript.
+
+
+## Built Withhttps://www.loom.com/share/940147d69ce84a048796b240cc10d1d9
+
+- Html5
+- CSS3
+- Javascript
+
+## Live Demo
+
+[Live Demo Link](https://www.loom.com/share/940147d69ce84a048796b240cc10d1d9)
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+> open your termina and do git clone https://github.com/Pazzo97/Capstone.git to have this project locally.
+
+
+## Author
+
+👤 **Author**
+
+- GitHub: [@Pazzo97](https://github.com/Pazzo97)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/patrick-mukunzi-8389861a9/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Pazzo97/Capstone/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Credit to  Cindy Shin (https://www.behance.net/adagio07) who designed this website
+- geeksforgeeks
+- stack overflow
+- medium
+- w3school
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
---
+++ script.js
@@ -0,0 +1,70 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+function close() {
+  mainMenu.style.top = '-120%';
+}
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+const featureLecturersList = [
+  {
+    image: 'images/Passport-Picture.JPG',
+    name: 'Patrick Mukunzi',
+    title: 'CEO Nobilis',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
+
+  {
+    image: 'images/Passport-Picture.JPG',
+    name: 'Patrick Mukunzi',
+    title: 'Faculty head EMU',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
+
+  {
+    image: 'images/Passport-Picture.JPG',
+    name: 'Patrick Mukunzi',
+    title: 'Lecturer Boston MIS',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
+
+  {
+    image: 'images/Passport-Picture.JPG',
+    name: 'Patrick Mukunzi',
+    title: 'Lecturer Texas Tech',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
+
+];
+
+const lecturersSection = document.getElementById('lecturers');
+lecturersSection.innerHTML = featureLecturersList.map((person) => `
+
+<div class="lecturer-box">
+
+<div class="lecturer-photo">
+  <img src="${person.image}" alt="Lecturer pic">
+</div>
+
+<div class="lecturer-bio">
+
+  <div class="lecturer-name">
+    <h3>${person.name}</h3>
+  </div>
+  <div class="lecturer-job">
+    <p>${person.title}</p>
+  </div>
+  <div class="lecturer-summary">
+    <p>${person.bio}</p>
+  </div>
+</div>
+
+</div>
+`).join('');
