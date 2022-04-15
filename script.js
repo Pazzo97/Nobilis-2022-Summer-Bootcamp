96f2bf6d3b2babb28c1bfce19df1ab4ccf49f117
Pazzo97 <pazzomuk@gmail.com>
2022-04-15 19:01:51 +0200

    fixing required changes

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ closeMenu.addEventListener('click', close);
 
 const featureLecturersList = [
   {
-    image: 'images/new.png',
+    image: 'images/Passport-Picture.JPG',
     name: 'Patrick Mukunzi',
     title: 'CEO Nobilis',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
   },
 
   {
-    image: 'images/new.png',
+    image: 'images/Passport-Picture.JPG',
     name: 'Patrick Mukunzi',
     title: 'Faculty head EMU',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
@@ -36,21 +36,21 @@ const featureLecturersList = [
   },
 
   {
-    image: 'images/new.png',
+    image: 'images/Passport-Picture.JPG',
     name: 'Patrick Mukunzi',
     title: 'Faculty head EMU',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
   },
 
   {
-    image: 'images/new.png',
+    image: 'images/Passport-Picture.JPG',
     name: 'Patrick Mukunzi',
     title: 'Lecturer Boston MIS',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
   },
 
   {
-    image: 'images/new.png',
+    image: 'images/Passport-Picture.JPG',
     name: 'Patrick Mukunzi',
     title: 'Lecturer Texas Tech',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
